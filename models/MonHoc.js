99916cc94e2cee0cc65698b3db18a168ccf0a582
tungphanv2868 <tungphanv2868@gmail.com>
2022-03-07 19:11:04 +0700

    devA_commit_monhoc

---
+++ models/MonHoc.js
@@ -0,0 +1,7 @@
+function MonHoc(){
+    this.maMonHoc = '';
+    this.tenMonHoc = '';
+    this.hienThiThongTinMonHoc = function(){
+        console.log('thong tin mon hoc',this.maMonHoc, this.tenMonHoc);
+    }
+}
